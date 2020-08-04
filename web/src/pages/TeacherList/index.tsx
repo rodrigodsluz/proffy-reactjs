16ccf8b54c5be404511063ec6ce8b000aa1a419a
rodrigodsluz <rodrigodsluz@gmail.com>
2020-08-04 12:07:53 -0300

    Add teachers

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
 import './styles.css'
 
 const TeacherList = () => {
@@ -25,6 +27,35 @@ const TeacherList = () => {
             </div>
           </form>
         </PageHeader>
+
+        <main>
+          <article className="teacher-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/52974568?s=460&u=1160314eec540c568cc199c88a708a66e9ae9bbf&v=4" alt="Rodrigo Duarte"/>
+              <div>
+                <strong>Rodrigo Duarte</strong>
+                <span>Physics</span>
+              </div>
+            </header>
+
+            <p>
+              Human being from Earth
+              <br/> <br/>
+              Rodrigo is a human being living in a planet called Earth located in the Solar System that's is located in the Milky Way
+            </p>
+
+            <footer>
+              <p>
+                Price/time
+                <strong>U$ 80,00</strong>
+              </p>
+              <button type="button">
+                <img src={whatsappIcon} alt="Whatsapp"/>
+                Get in touch
+              </button>
+            </footer>
+          </article>
+        </main>
       </div>
     )
 }
