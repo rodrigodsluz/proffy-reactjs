ea42805db71ba74123f8ccc6c4b2240175f996c7
rodrigodsluz <rodrigodsluz@gmail.com>
2020-08-04 13:07:36 -0300

    Add teacher item component

---
+++ web/src/components/TeacherItem/index.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+
+import './styles.css'
+
+const TeacherItem = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img src="https://avatars2.githubusercontent.com/u/52974568?s=460&u=1160314eec540c568cc199c88a708a66e9ae9bbf&v=4" alt="Rodrigo Duarte"/>
+        <div>
+          <strong>Rodrigo Duarte</strong>
+          <span>Physics</span>
+        </div>
+      </header>
+
+      <p>
+        Human being from Earth
+        <br/> <br/>
+        Rodrigo is a human being living in a planet called Earth located in the Solar System that's is located in the Milky Way
+      </p>
+
+      <footer>
+        <p>
+          Price/time
+          <strong>U$ 80,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="Whatsapp"/>
+          Get in touch
+        </button>
+      </footer>
+    </article>
+  )
+}
+
+export default TeacherItem
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 
 import PageHeader from '../../components/PageHeader'
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import TeacherItem from '../../components/TeacherItem'
 
 import './styles.css'
 
@@ -29,32 +28,10 @@ const TeacherList = () => {
         </PageHeader>
 
         <main>
-          <article className="teacher-item">
-            <header>
-              <img src="https://avatars2.githubusercontent.com/u/52974568?s=460&u=1160314eec540c568cc199c88a708a66e9ae9bbf&v=4" alt="Rodrigo Duarte"/>
-              <div>
-                <strong>Rodrigo Duarte</strong>
-                <span>Physics</span>
-              </div>
-            </header>
-
-            <p>
-              Human being from Earth
-              <br/> <br/>
-              Rodrigo is a human being living in a planet called Earth located in the Solar System that's is located in the Milky Way
-            </p>
-
-            <footer>
-              <p>
-                Price/time
-                <strong>U$ 80,00</strong>
-              </p>
-              <button type="button">
-                <img src={whatsappIcon} alt="Whatsapp"/>
-                Get in touch
-              </button>
-            </footer>
-          </article>
+          <TeacherItem />
+          <TeacherItem />
+          <TeacherItem />
+          <TeacherItem />
         </main>
       </div>
     )
