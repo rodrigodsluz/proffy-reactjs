f6b3cb2d0f4b67eb5d49ef18f80ad522e1a0f015
rodrigodsluz <rodrigodsluz@gmail.com>
2020-08-04 09:42:30 -0300

    Add teacher list

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import PageHeader from '../../components/PageHeader'
+
 const TeacherForm = () => {
   return (
-    <h1>Teacher form</h1>
+    <div id="page-teacher-form" className="container">
+      <PageHeader title="How amazing you want to teach"/>
+    </div>
   )
 }
 
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,26 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
-import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import PageHeader from '../../components/PageHeader'
 
 import './styles.css'
 
 const TeacherList = () => {
   return (
       <div id="page-teacher-list" className="container">
-        <header className="page-header">
-          <div className="top-bar-container">
-            <Link to="/">
-              <img src={backIcon} alt="Back"/>
-            </Link>
-            <img src={logoImg} alt=""/>
-          </div>
+        <PageHeader title="These are the available teachers">
+          <form id="search-teachers">
+            <div className="input-block">
+              <label htmlFor="subject">Subject</label>
+              <input type="text" id="subject"/>
+            </div>
 
-          <div className="header-content">
-            <strong>These are the available teachers</strong>
-          </div>
-        </header>
+            <div className="input-block">
+              <label htmlFor="week-day">Week day</label>
+              <input type="text" id="week-day"/>
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="time">Time</label>
+              <input type="text" id="time"/>
+            </div>
+          </form>
+        </PageHeader>
       </div>
     )
 }
