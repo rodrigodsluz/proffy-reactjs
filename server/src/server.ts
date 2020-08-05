c763eb131e4a2d3e85478d75a9766d8104c95a47
rodrigodsluz <rodrigodsluz@gmail.com>
2020-08-05 10:27:38 -0300

    Add beginning

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,15 +4,8 @@ const app = express()
 
 app.use(express.json())
 
-app.post('/users', (request, response) => {
-  console.log(request.body)
-
-  const users = [
-  { name: 'Rodrigo', age: 25},
-  { name: 'Sofia', age: 21},
-  ]
-
-  return response.json(users)
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello world' })
 })
 
 app.listen(3333)
