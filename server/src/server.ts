b0184fed0015ade175f6761b92096da02a1680e8
rodrigodsluz <rodrigodsluz@gmail.com>
2020-08-05 09:42:13 -0300

    Add route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-console.log('Hello world') 
+import express from 'express'
+
+const app = express()
+
+app.get('/users', (request, response) => {
+  const users = [
+  { name: 'Rodrigo', age: 25},
+  { name: 'Sofia', age: 21},
+  ]
+
+  return response.json(users)
+})
+
+app.listen(3333)
