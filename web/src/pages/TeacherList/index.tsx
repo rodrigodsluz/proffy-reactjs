789ff89c8d4a33683c912d74ec5dc6b00421a3f1
rodrigodsluz <rodrigodsluz@gmail.com>
2020-11-03 09:57:03 -0300

    Update teacher list  component

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,40 +1,54 @@
-import React from 'react'
+import React from "react";
+import "./styles.css";
 
-import PageHeader from '../../components/PageHeader'
-import TeacherItem from '../../components/TeacherItem'
+import PageHeader from "../../components/PageHeader/";
+import TeacherItem from "../../components/TeacherItem";
+import Input from "../../components/Input";
+import Select from "../../components/Select";
 
-import './styles.css'
-
-const TeacherList = () => {
+export default function TeacherList() {
   return (
-      <div id="page-teacher-list" className="container">
-        <PageHeader title="These are the available teachers">
-          <form id="search-teachers">
-            <div className="input-block">
-              <label htmlFor="subject">Subject</label>
-              <input type="text" id="subject"/>
-            </div>
+    <div className="container" id="page-teacher-list">
+      <PageHeader title="Esses são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: "Artes", label: "Artes" },
+              { value: "Biologia", label: "Biologia" },
+              { value: "Geografia", label: "Geografia" },
+              { value: "Matematica", label: "Matematica" },
+              { value: "Ingles", label: "Ingles" },
+              { value: "Historia", label: "Historia" },
+              { value: "Fisica", label: "Fisica" },
+              { value: "Portugues", label: "Portugues" },
+              { value: "Quimica", label: "Quimica" },
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Dia da Semana"
+            options={[
+              { value: "0", label: "Domingo" },
+              { value: "1", label: "Segunda" },
+              { value: "2", label: "Terça" },
+              { value: "3", label: "Quarta" },
+              { value: "4", label: "Quinta" },
+              { value: "5", label: "Sexta" },
+              { value: "6", label: "Sábado" },
+            ]}
+          />
 
-            <div className="input-block">
-              <label htmlFor="week-day">Week day</label>
-              <input type="text" id="week-day"/>
-            </div>
+          <Input name="time" label="Hora" type="time" />
+        </form>
+      </PageHeader>
 
-            <div className="input-block">
-              <label htmlFor="time">Time</label>
-              <input type="text" id="time"/>
-            </div>
-          </form>
-        </PageHeader>
-
-        <main>
-          <TeacherItem />
-          <TeacherItem />
-          <TeacherItem />
-          <TeacherItem />
-        </main>
-      </div>
-    )
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  );
 }
-
-export default TeacherList
