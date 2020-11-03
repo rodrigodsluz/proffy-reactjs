0b03fb97ef5cc009004c2a5cca478c56e70d5677
rodrigodsluz <rodrigodsluz@gmail.com>
2020-11-03 11:37:39 -0300

    Start connect back-end with front-end

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,48 +1,59 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { ReactElement, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
-import logoImg from '../../assets/images/logo.svg'
-import landingImg from '../../assets/images/landing.svg'
+import './styles.css';
 
-import studyIcon from '../../assets/images/icons/study.svg'
-import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
-import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
+import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
 
-import './styles.css'
+import studyIcon from '../../assets/images/icons/study.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
+import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
-const Landing = () => {
-  return (  
+function Landing(): ReactElement {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+ 
+  return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
         <div className="logo-container">
-          <img src={logoImg} alt="Proffy"/>
-          <h2>Your online study platform</h2>
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
         </div>
-
-        <img 
-          src={landingImg} 
-          alt="Study platform" 
-          className="hero-image"
-        />
+        <img src={landingImg} alt="Plataforma de estudos" className="hero-image" />
 
         <div className="buttons-container">
           <Link to="/study" className="study">
-            <img src={studyIcon} alt="study"/>
-            Study
+            <img src={studyIcon} alt="Estudar" />
+            Estudar
           </Link>
 
           <Link to="/give-classes" className="give-classes">
-            <img src={giveClassesIcon} alt="Give classes"/>
-            Give classes
+            <img src={giveClassesIcon} alt="Dar aulas" />
+            Dar aulas
           </Link>
         </div>
 
         <span className="total-connections">
-          Total of 200 connections already made <img src={purpleHeartIcon} alt="Purple heart"/>
+          Total de 
+          {' '}
+          {totalConnections}
+          {' '}
+          conexões já realizadas
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
-  )
+  );
 }
 
-export default Landing
+export default Landing;
