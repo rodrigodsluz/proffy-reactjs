210cca40a305e69f72fe644235dcfcb8c6053879
rodrigodsluz <rodrigodsluz@gmail.com>
2020-11-07 07:07:08 -0300

    Add rect button

---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+
+import styles from './styles';
+
+import giveCLassesBgImage from '../../assets/images/give-classes-background.png';
+
+function GiveClasses() {
+  const { goBack } = useNavigation();
+
+  function handleNavigateBack() {
+    goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode=}
+
+export default GiveClasses;
